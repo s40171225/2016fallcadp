9c72f906d90dee1556f1f5f938c981f9156f0386
機械設計工程系課程 <mdecourse@gmail.com>
2016-08-12 07:55:13 +0800

    Update syllabus.py

--- syllabus.py
+++ syllabus.py
@@ -2,8 +2,9 @@
 # 二十一世紀的機械設計產品開發, 必須能夠直接在網路上進行協同設計, 系統與產品都必須納入產品生命週期管理外, 所有工具都應該能夠隨著需求變更而持續改進.
 # syllabus.py
 # 各學員利用學校所發的 Gmail 帳號, 以學號作為帳號申請 github 帳號
-# 期中考週前, 主要實習項目為 task1.py, 目標為組裝一台 Kossel Delta 3D printer, 並利用 Solvespace, OnShape, Creo Parametric, Solidworks 建立零組件ㄡ
-# 期中考週之後, 則以開發一套能讓所有課程組員進行網際協同設計的系統, 以能夠完成本課程之協同課程評量任務為例, 設為本課程之 task2.py.
+# 期中考週前, 主要實習項目為 task1.py, 目標為組裝一台 Kossel Delta 3D printer, 並利用 Solvespace, OnShape, Creo Parametric, Solidworks 建立零組件
+# 期中考週之後, 則以開發一套能讓所有課程組員進行網際協同設計的系統, 以能夠完成本課程之協同課程評量任務為例, 設為本課程之 task2.py
+# 系統將採 github 帳號登入，且登入後可進行協同設計程式開發 (Flask+redis+GitHub api), 協同網誌編寫，自評及組內互評等工作 (Flask+SQLite+MySQL+GitHub api).
 # 各組可自選組員每組 4-7 人, 所有學員在電腦輔助設計室中的座位採電腦程式編排座位, 且該程式的開發設為計算機程式課程範例 task1.py
 '''
 電腦輔助設計實習課程宣言:
